6dd532cb2f89b75f8883acbfd4b23801a512e765
turab45 <turabbajeer202@gmail.com>
2021-05-30 10:54:20 +0500

    Add database connectivity

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'title': 'Blog Post 1',
-        'author': 'Turab45',
-        'content': 'This is the first post',
-        'date_posted': 'May 30, 2020'
-    },
-    {
-        'title': 'Blog Post 2',
-        'author': 'Turab Bajeer',
-        'content': 'This is the Second post',
-        'date_posted': 'June 1, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
